2e58bb90187722a07e3cea8bbda034006b0a3745
Dulshani Gunawardhana <degunawardhana@gmail.com>
2016-03-18 21:17:04 +0530

    Compiled JSX

--- js/index.js
+++ js/index.js
@@ -1,77 +1,132 @@
-const { Router, Route, IndexRoute, Redirect, Link, IndexLink} = ReactRouter
+"use strict";
+
+var _ReactRouter = ReactRouter;
+var Router = _ReactRouter.Router;
+var Route = _ReactRouter.Route;
+var IndexRoute = _ReactRouter.IndexRoute;
+var Redirect = _ReactRouter.Redirect;
+var Link = _ReactRouter.Link;
+var IndexLink = _ReactRouter.IndexLink;
 
 var Header = React.createClass({
-  render: function() {
-    return (
-      <div className="header">
-        <h1><Link to={`/`} id="title">Dulshani Gunawardhana</Link></h1>
-      </div>
+  displayName: "Header",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "header" },
+      React.createElement(
+        "h1",
+        null,
+        React.createElement(
+          Link,
+          { to: "/", id: "title" },
+          "Dulshani Gunawardhana"
+        )
+      )
     );
   }
 });
 
 var About = React.createClass({
-  render: function() {
-    return (
-      <div className="header">
-        <h3><Link to={`/`} id="title">Dulshani Gunawardhana</Link></h3>
-      </div>
+  displayName: "About",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "header" },
+      React.createElement(
+        "h3",
+        null,
+        React.createElement(
+          Link,
+          { to: "/", id: "title" },
+          "Dulshani Gunawardhana"
+        )
+      )
     );
   }
 });
 
-const App = React.createClass({
-  getInitialState: function() {
-    return {data: []};
+var App = React.createClass({
+  displayName: "App",
+
+  getInitialState: function getInitialState() {
+    return { data: [] };
   },
-  componentDidMount: function() {
+  componentDidMount: function componentDidMount() {
     $.ajax({
       url: "http://dshgna.github.io/posts.json",
       dataType: 'json',
       cache: true,
-      success: function(data) {
-        this.setState({data: data.reverse()});
+      success: function (data) {
+        this.setState({ data: data.reverse() });
       }.bind(this),
-      error: function(xhr, status, err) {
+      error: function (xhr, status, err) {
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
   },
-  render: function() { 
-    var childrenWithProps = React.Children.map(this.props.children, (child) => {
-      return React.cloneElement(child, { data: this.state.data });
+  render: function render() {
+    var _this = this;
+
+    var childrenWithProps = React.Children.map(this.props.children, function (child) {
+      return React.cloneElement(child, { data: _this.state.data });
     });
-    return (
-      <div class="container-fluid">
-        <div id ="sidebar"><Header /></div>
-        <main><div id="content">{childrenWithProps}</div></main>
-      </div>
-  )}
+    return React.createElement(
+      "div",
+      { "class": "container-fluid" },
+      React.createElement(
+        "div",
+        { id: "sidebar" },
+        React.createElement(Header, null)
+      ),
+      React.createElement(
+        "main",
+        null,
+        React.createElement(
+          "div",
+          { id: "content" },
+          childrenWithProps
+        )
+      )
+    );
+  }
 });
 
-const Tag = React.createClass({
-  render: function() {
-    var tagNodes = this.props.tag.map(function(t) {
-      return(
-        <p className='label label-success'>{t}</p>
-      );});
-    return (
-      <div className='tag'>{tagNodes}</div>
-    );}
+var Tag = React.createClass({
+  displayName: "Tag",
+
+  render: function render() {
+    var tagNodes = this.props.tag.map(function (t) {
+      return React.createElement(
+        "p",
+        { className: "label label-success" },
+        t
+      );
+    });
+    return React.createElement(
+      "div",
+      { className: "tag" },
+      tagNodes
+    );
+  }
 });
 
-const Post = React.createClass({
-  getInitialState: function() {
-    return {data: []};
+var Post = React.createClass({
+  displayName: "Post",
+
+  getInitialState: function getInitialState() {
+    return { data: [] };
   },
-  componentDidMount: function() {
-    const id = this.props.params.id;
-    const info = this.props.data;
+  componentDidMount: function componentDidMount() {
+    var id = this.props.params.id;
+    var info = this.props.data;
     var path = 'http://dshgna.github.io/posts/' + info[id]["link"];
     $.ajax({
       url: path,
       cache: false,
-      success: function(data) {
+      success: function (data) {
         this.setState({
           title: info[id]["title"],
           date: info[id]["date"],
@@ -79,51 +134,83 @@ const Post = React.createClass({
           data: data
         });
       }.bind(this),
-      error: function(xhr, status, err) {
+      error: function (xhr, status, err) {
         console.error(path, status, err.toString());
       }.bind(this)
     });
   },
-  rawMarkup: function() {
-    var rawMarkup = marked(this.state.data.toString(), {sanitize: true});
+  rawMarkup: function rawMarkup() {
+    var rawMarkup = marked(this.state.data.toString(), { sanitize: true });
     return { __html: rawMarkup };
   },
-  render: function() {
-    return (
-      <div className="post">
-        <h1>{this.state.title}</h1>
-        <p className='date'>{this.state.date}</p><br/>
-        <span dangerouslySetInnerHTML={this.rawMarkup()} />
-      </div>
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "post" },
+      React.createElement(
+        "h1",
+        null,
+        this.state.title
+      ),
+      React.createElement(
+        "p",
+        { className: "date" },
+        this.state.date
+      ),
+      React.createElement("br", null),
+      React.createElement("span", { dangerouslySetInnerHTML: this.rawMarkup() })
     );
   }
 });
 
+var PostList = React.createClass({
+  displayName: "PostList",
 
-const PostList = React.createClass({
-  render: function() { 
-    var postNodes = this.props.data.map(function(post, index){
-      return (
-        <div className="post">
-          <h2 className='text-center'>{post.title}</h2>
-          <Tag tag={post.tags}/>
-          <p className='date text-center'>{post.date}</p><br/>
-          <p>{post.description}</p>
-          <Link to={`posts/${index}`} id="read-more">Read More-></Link>
-        </div>
-        );});
-    return (
-      <div className="content">{postNodes}</div>
-    );}
+  render: function render() {
+    var postNodes = this.props.data.map(function (post, index) {
+      return React.createElement(
+        "div",
+        { className: "post" },
+        React.createElement(
+          "h2",
+          { className: "text-center" },
+          post.title
+        ),
+        React.createElement(Tag, { tag: post.tags }),
+        React.createElement(
+          "p",
+          { className: "date text-center" },
+          post.date
+        ),
+        React.createElement("br", null),
+        React.createElement(
+          "p",
+          null,
+          post.description
+        ),
+        React.createElement(
+          Link,
+          { to: "posts/" + index, id: "read-more" },
+          "Read More->"
+        )
+      );
+    });
+    return React.createElement(
+      "div",
+      { className: "content" },
+      postNodes
+    );
+  }
 });
 
-
-ReactDOM.render((
-  <Router>
-    <Route path="/" component={App}>
-      <IndexRoute component={PostList}/>
-      <Route path="posts/:id" component={Post}/>
-      <Route path="about" component={About}/>
-    </Route>
-  </Router>
-), document.body)
+ReactDOM.render(React.createElement(
+  Router,
+  null,
+  React.createElement(
+    Route,
+    { path: "/", component: App },
+    React.createElement(IndexRoute, { component: PostList }),
+    React.createElement(Route, { path: "posts/:id", component: Post }),
+    React.createElement(Route, { path: "about", component: About })
+  )
+), document.body);
